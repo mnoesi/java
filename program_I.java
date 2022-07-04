c51e190de7433687d848bf931deb9d8416365238
Michael Noesi <37088807+mnoesi@users.noreply.github.com>
2022-07-04 14:57:56 -0500

    Update program_I.java

--- program_I.java
+++ program_I.java
@@ -1,7 +1,7 @@
 //Write, test, and compile a Java program that outputs the string "Hello World!"
 
-class program{
+public class program{
   public static void main(String[] a){
-    System.out.print("Hello World!");
+    System.out.print("\nHello World!");
   }
 }
