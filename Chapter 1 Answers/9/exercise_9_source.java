a7f2f56c559c7e979ed1273180ea2dcdf9c226d7
Michael Noesi <37088807+mnoesi@users.noreply.github.com>
2022-12-04 20:03:43 -0600

    Add files via upload

---
+++ Chapter 1 Answers/9/exercise_9_source.java
@@ -0,0 +1,11 @@
+public class Program {
+    public static void main(String a[]){
+        System.out.print("      T\n");
+        System.out.print("     TTT\n");
+        System.out.print("    TTTTT\n");
+        System.out.print("   TTTTTTT\n");
+        System.out.print("  TTTTTTTTT\n");
+        System.out.print(" TTTTTTTTTTT\n");
+        System.out.print("TTTTTTTTTTTTT\n");
+    }
+}
